d55365effd4bc893637864bb5dd16ed7c8a31088
Richard Protasov <richardprotasov@gmail.com>
2016-04-17 16:55:27 -0400

    🎳 users migration support additional fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,20 +3,23 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsersTable extends Migration 
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up() 
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('email_confirmed');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('password');
+            $table->boolean('remember_me');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -27,7 +30,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down() 
     {
         Schema::drop('users');
     }
