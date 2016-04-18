509889f64cadd526a0a9b9bd568dfb7031533980
Richard Protasov <richardprotasov@gmail.com>
2016-04-18 02:50:33 -0400

    💅 customises AuthController and UserModel to what we have so far

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'email', 'fist_name', 'last_name', 'password',
+        'email', 'first_name', 'last_name', 'password',
     ];
 
     /**
