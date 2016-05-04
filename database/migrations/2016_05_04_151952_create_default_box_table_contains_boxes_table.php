b8453269decdcc95eadd1a7ced8a40bcb35046f4
Richard Protasov <richardprotasov@gmail.com>
2016-05-04 11:24:55 -0400

    🏇 Runs migrations for default boxes along with its related relationships

---
+++ database/migrations/2016_05_04_151952_create_default_box_table_contains_boxes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDefaultBoxTableContainsBoxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('default_box_table_contains_boxes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('default_box_id')->unsigned();
+            $table->integer('box_id')->unsigned();
+            $table->foreign('default_box_id')
+                  ->references('id')->on('default_boxes')
+                  ->onDelete('cascade');
+            $table->foreign('box_id')
+                  ->references('id')->on('boxes')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('default_box_table_contains_boxes');
+    }
+}
