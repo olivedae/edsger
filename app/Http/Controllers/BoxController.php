4fbe071d8c8fda0ecac35c139aad01719ace371f
Richard Protasov <richardprotasov@gmail.com>
2016-05-05 11:37:57 -0400

    Migrates BoxPermissionRepo to BoxRep correctly (can create new boxes again)

--- app/Http/Controllers/BoxController.php
+++ app/Http/Controllers/BoxController.php
@@ -2,34 +2,103 @@
 
 namespace App\Http\Controllers;
 
-use App\Box;
+use App\BoxPermission;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Repositories\BoxRepository;
+use App\Box;
+use App\DefaultBox;
+use App\DefaultBoxContainsBoxes;
 
 class BoxController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * The box repository instance.
      *
-     * @return void
+     * @var BoxRepository
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
+    protected $boxes;
 
     /**
-     * Create a new Box - only used for the owner
-     *      
+     * Create a new controller instance.
+     *
+     * @param  BoxPermissionRepository  $permissions
+     * @return void
+     */
+    public function __construct(BoxRepository $boxes)
+    {
+        $this->middleware('auth');
+        $this->boxes = $boxes;
+    }
+
+    /**
+     * Create a new entry in box_permissions, which
+     *     is the default for owners making a new
+     *     box.
+     *
      * @param Request $request
-     * @return Response
+     * @return BoxPermission
      */
     public function store(Request $request)
     {
-        return Box::create([
-            'name' => $request->name
+        $this->validate($request, [
+            'name' => 'required|max:255',
         ]);
+
+        $box = Box::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'in_default_box' => true
+        ]);
+
+        $user = $request->user();
+
+        $permission = BoxPermission::create([
+            'user_id' => $user->id,
+            'box_id' => $box->id,
+            'is_owner' => true,
+            'can_edit' => true,
+        ]);
+
+        $defaultBox =
+            DefaultBox::where('user_id', $request->user()->id)
+                ->first();
+
+        DefaultBoxContainsBoxes::create([
+            'box_id' => $box->id,
+            'default_box_id' => $defaultBox->id,
+        ]);
+
+        return redirect('dashboard');
     }
+
+    /**
+    * Display a view for creating new boxes
+    *
+    * @param  Request  $request
+    * @return Response
+    */
+   public function new(Request $request)
+   {
+       return view('boxes.new');
+   }
+
+   /**
+    * Deletes an entry in boxes if and
+    *     only if the user deleteing
+    *     the box is the owner of it.
+    *
+    * @param Request $request
+    * @param Box $box
+    * @return Response
+    */
+   public function destroy(Request $request, Box $box)
+   {
+       $this->authorize('destroy', $box);
+
+       $box->delete();
+
+       return redirect('dashboard');
+   }
 }
---
+++ app/Repositories/BoxRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+use App\BoxPermission;
+
+class BoxRepository
+{
+    /**
+     * Get all of the permissions for a given user
+     *
+     * @param User $user
+     * @return Collection
+     */
+    public function forUser(User $user)
+    {
+        $permissions =
+            BoxPermission::where('user_id', $user->id)
+                ->orderBy('created_at', 'asc')
+                ->get();
+
+        $boxes = $permissions->map(function($item, $key) {
+            return $item->box();
+        });
+
+        return $boxes;
+    }
+
+    /**
+     * Get all the users with permissions for a
+     *     given box
+     *
+     * @param Box $box
+     * @return Collection
+     */
+    public function forBox(Box $box)
+    {
+        $perissions =
+            BoxPermission::where('box_id', $box->id)
+                ->orderBy('created_at', 'asc')
+                ->get();
+
+        $users = $permissions->map(function($item, $key) {
+            return $item->user();
+        });
+
+        return $users;
+    }
+}
